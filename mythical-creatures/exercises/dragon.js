70d0e5992d386d17a4106c5ee28b98338da961ed
Race Osuna <raceosuna@gmail.com>
2023-02-06 21:31:26 -0700

    Feat: dragon class

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,24 @@
+class Dragon {
+    constructor(name, rider) {
+      this.name = name;
+      this.rider = rider;
+      this.hungry = true;
+      this.snack = 0
+    }
 
+    greet() {
+        return `Hi, ${this.rider}!`;
+    }
+
+    eat() {
+        this.snack += 1;
+        if (this.snack > 2) {
+            this.hungry = false
+        }
+
+    }
+    
+
+}
+
+module.exports = Dragon;
