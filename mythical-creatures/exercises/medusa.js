33a070a753674a1c70338d0f0fb8739c10af9464
Race Osuna <raceosuna@gmail.com>
2023-02-13 22:36:38 -0700

    Feat: medusa, person and statue classes

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,26 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+    victim = new Statue(victim.name);
+    this.statues.push(victim)
+  if (this.statues.length > 3) {
+    victim = this.statues.shift()
+    victim = new Person(victim.name)
+    victim.mood = "relieved"
+  return victim
+  }
+ }
+}
+
+
+
+
+
+module.exports = Medusa;
